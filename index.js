bc2d43f3f501060b337451c5f86b4530e269da5f
aashaynagrale18898 <65070431+aashaynagrale18898@users.noreply.github.com>
2024-06-26 06:20:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+
+var randomNumber1 = Math.floor(Math.random()*6) +1;
+
+var imageSelect = "dice"+ randomNumber1 +".png";
+
+var imageSource = "images/"+ imageSelect;
+
+var image1 = document.querySelectorAll("img")[0].setAttribute("src", imageSource);
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+
+var imageSelect = "dice"+ randomNumber2 +".png";
+
+var imageSource = "images/"+ imageSelect;
+
+var image2 = document.querySelectorAll("img")[1].setAttribute("src", imageSource);
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 wins!";
+}
+else if (randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 2 wins!!"
+}
+else{
+    document.querySelector("h1").innerHTML = "It's a draw."
+}
